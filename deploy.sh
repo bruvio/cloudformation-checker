5bb18ba7a35046d65db97ed9ca132c7d6ee568d8
bruvio <bruno.viola@genomicsengland.co.uk>
2024-09-26 06:46:06 +0100

    fix: adding additional tests using a simple bucket stack

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
+
 # Retrieve AWS Account ID
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 TEMPLATE_DIR="/Users/brunoviola/WORK/cloudformation-checker"
 AWS_REGION="eu-west-2"
 export AWS_REGION
-if [ -z "$AWS_ACCOUNT_ID" ]; then
-  echo "Failed to retrieve AWS Account ID."
-  exit 1
-fi
+
 
 echo "AWS Account ID: $AWS_ACCOUNT_ID"
 
@@ -17,7 +15,20 @@ echo "AWS Account ID: $AWS_ACCOUNT_ID"
 echo "Creating S3 bucket: $S3BucketName"
 export S3BucketName="cloudformation-cheker-bruvio-${AWS_ACCOUNT_ID}"
 
-echo "Creating S3 bucket: $S3BucketName"
+
+echo "bucket names used for testing:"
+
+VALID_BUCKET_NAME="this-is-a-valid-name-${AWS_ACCOUNT_ID}"
+echo "valid bucket name: $VALID_BUCKET_NAME"
+
+
+# Generate an invalid bucket name by including underscores
+INVALID_BUCKET_NAME="invalid_bucket_name_with_underscores_${AWS_ACCOUNT_ID}"
+echo "Invalid bucket name to trigger rollback: $INVALID_BUCKET_NAME"
+
+echo ""
+
+echo "Creating S3 bucket to store CF templates: $S3BucketName"
 # Create the S3 bucket
 # Attempt to create the S3 bucket
 CREATE_BUCKET_OUTPUT=$(aws s3 mb s3://$S3BucketName --region eu-west-2 2>&1)
@@ -29,7 +40,7 @@ if [ $CREATE_BUCKET_EXIT_CODE -ne 0 ]; then
     echo "Bucket already exists and is owned by you. Continuing..."
   elif echo "$CREATE_BUCKET_OUTPUT" | grep -q 'BucketAlreadyExists'; then
     echo "Bucket already exists but is owned by someone else. Please choose a different bucket name."
-    exit 1
+
   else
     echo "Failed to create S3 bucket."
     echo "Error: $CREATE_BUCKET_OUTPUT"
@@ -37,13 +48,15 @@ if [ $CREATE_BUCKET_EXIT_CODE -ne 0 ]; then
   fi
 fi
 
+echo ""
 
 
-# Upload your templates to the bucket
+echo "Upload templates to the bucket $S3BucketName"
 aws s3 cp "$TEMPLATE_DIR/parent-stack.yml" s3://$S3BucketName/ --region $AWS_REGION
 aws s3 cp "$TEMPLATE_DIR/nested-stack-1.yml" s3://$S3BucketName/ --region $AWS_REGION
 aws s3 cp "$TEMPLATE_DIR/nested-stack-2.yml" s3://$S3BucketName/ --region $AWS_REGION
 aws s3 cp "$TEMPLATE_DIR/nested-stack-3.yml" s3://$S3BucketName/ --region $AWS_REGION
+aws s3 cp "$TEMPLATE_DIR/simple-s3-bucket.yml" s3://$S3BucketName/ --region $AWS_REGION
 
 if [ $? -ne 0 ]; then
   echo "Failed to upload templates to S3 bucket."
@@ -51,12 +64,13 @@ if [ $? -ne 0 ]; then
 fi
 aws s3 ls s3://$S3BucketName
 
-# Generate an invalid bucket name by including underscores
-INVALID_BUCKET_NAME="invalid_bucket_name_with_underscores_${AWS_ACCOUNT_ID}_$(date +%s)"
-echo "Invalid bucket name to trigger rollback: $INVALID_BUCKET_NAME"
 
 
-# Deploy the parent stack
+
+echo""
+
+echo "Deploy the parent stack: ParentStack"
+
 aws cloudformation create-stack \
   --stack-name ParentStack \
   --template-url https://$S3BucketName.s3.$AWS_REGION.amazonaws.com/parent-stack.yml \
@@ -67,22 +81,63 @@ aws cloudformation create-stack \
     ParameterKey=NestedStack3BucketName,ParameterValue=$INVALID_BUCKET_NAME \
   --capabilities CAPABILITY_NAMED_IAM \
   --region $AWS_REGION
-
-
-# Check if the stack creation command was successful
 if [ $? -ne 0 ]; then
   echo "Failed to initiate CloudFormation stack creation."
   exit 1
 fi
 
-echo "CloudFormation stack creation initiated successfully."
-
-aws cloudformation list-stacks
 
 
-aws cloudformation describe-stacks --stack-name ParentStack
+
+echo ""
+echo "provision valid s3 bucket - stackname MyS3BucketStack"
+aws cloudformation create-stack \
+    --stack-name MyS3BucketStack \
+    --template-url https://$S3BucketName.s3.$AWS_REGION.amazonaws.com/simple-s3-bucket.yml \
+    --parameters ParameterKey=S3BucketName,ParameterValue=$VALID_BUCKET_NAME \
+    --region $AWS_REGION
+
+if [ $? -ne 0 ]; then
+  echo "Failed to initiate CloudFormation stack creation."
+  exit 1
+fi
+
+echo ""
+echo "provision invalid s3 bucket - stackname MyInvalidS3BucketStack"
+aws cloudformation create-stack \
+    --stack-name MyInvalidS3BucketStack \
+    --template-url https://$S3BucketName.s3.$AWS_REGION.amazonaws.com/simple-s3-bucket.yml \
+    --parameters ParameterKey=S3BucketName,ParameterValue=$INVALID_BUCKET_NAME \
+    --region $AWS_REGION
+if [ $? -ne 0 ]; then
+  echo "Failed to initiate CloudFormation stack creation."
+  exit 1
+fi
 
 
+echo "done"
+
+return
+
+
+echo "checking the nested stack"
 python cf_status_checker.py ParentStack --region eu-west-2
 
+echo "checking the good bucket stack"
+python cf_status_checker.py MyS3BucketStack --region eu-west-2
+
+echo "checking the invalid bucket stack"
+python cf_status_checker.py MyInvalidS3BucketStack --region eu-west-2
+
+
+
+
+echo "deleting stacks"
+
+aws cloudformation delete-stack \
+    --stack-name ParentStack 
+aws cloudformation delete-stack \
+    --stack-name MyS3BucketStack 
+aws cloudformation delete-stack \
+    --stack-name MyInvalidS3BucketStack 
 
